1da0841cc3b55acf06a3b89c7bf2a0217c655f25
Bishworaj Poudel <iambrp.tech@gmail.com>
2025-02-24 19:40:08 +0545

    Group 1 Completed

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Contact
+from .forms import ContactForm
+
 # Create your views here.
 def index(request):
-    return render(request,'mainapp/index.html')
-
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Show js alert
+            return HttpResponse('<script>alert("Your message has been sent successfully!");</script>')
+    else:
+        form = ContactForm()
+    return render(request, 'mainapp/index.html', {'form': form})
